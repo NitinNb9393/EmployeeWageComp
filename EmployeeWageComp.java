df412ac40a166702fb17e4aba419008098ce7766
NitinNb9393 <nitinbabar9393@gmail.com>
2021-10-20 15:16:20 +0530

    Added:Check part time employee and wage

--- EmployeeWageComp.java
+++ EmployeeWageComp.java
@@ -9,6 +9,7 @@ public class EmployeeWageComp {
 		System.out.println("Welcome Employy Wage Computation problem");
 		toEmployeeIsPresent();
 		toCalculationDailyEmployeeWage();
+		toPartTimeEmployeeAndWage();
 	}
 
 	public static void toEmployeeIsPresent() {
@@ -36,5 +37,28 @@ public class EmployeeWageComp {
 
 		daily_wage = working_hr * wage_per_hr;
 		System.out.println("Employee daily wage is " + daily_wage);
+	}
+
+	public static void toPartTimeEmployeeAndWage() {
+		int is_fulltime_present = 1;
+		int is_parttime_present = 2;
+		int daily_wage;
+		int wage_per_hr = 20;
+		int working_hr;
+		int emp_check = (int) (Math.floor(Math.random() * 10)) % 3;
+		if (emp_check == is_fulltime_present) {
+			System.out.println("Employee is full time present");
+			working_hr = 8;
+		} else if (emp_check == is_parttime_present) {
+			System.out.println("Employee is part time present");
+			working_hr = 4;
+		} else {
+			System.out.println("Employee is absent");
+			working_hr = 0;
+		}
+
+		daily_wage = working_hr * wage_per_hr;
+		System.out.println("Employee daily wage is " + daily_wage);
+
 	}
 }
