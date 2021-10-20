c73a24e8b1691fef21efa0f4946b3e85a8c561e5
NitinNb9393 <nitinbabar9393@gmail.com>
2021-10-20 15:03:44 +0530

    Added:Calculation daily employee wage

--- EmployeeWageComp.java
+++ EmployeeWageComp.java
@@ -1,9 +1,14 @@
 package com.bridgelabz.employeewagecomp;
 
+import java.util.Scanner;
+
 public class EmployeeWageComp {
+	static Scanner scanner = new Scanner(System.in);
+
 	public static void main(String[] args) {
 		System.out.println("Welcome Employy Wage Computation problem");
 		toEmployeeIsPresent();
+		toCalculationDailyEmployeeWage();
 	}
 
 	public static void toEmployeeIsPresent() {
@@ -15,4 +20,21 @@ public class EmployeeWageComp {
 			System.out.println("Employee is absent");
 	}
 
+	public static void toCalculationDailyEmployeeWage() {
+		int is_fulltime_present = 1;
+		int daily_wage;
+		int wage_per_hr = 20;
+		int working_hr;
+		int emp_check = (int) (Math.floor(Math.random() * 10)) % 2;
+		if (emp_check == is_fulltime_present) {
+			System.out.println("Employee is present");
+			working_hr = 8;
+		} else {
+			System.out.println("Employee is absent");
+			working_hr = 0;
+		}
+
+		daily_wage = working_hr * wage_per_hr;
+		System.out.println("Employee daily wage is " + daily_wage);
+	}
 }
